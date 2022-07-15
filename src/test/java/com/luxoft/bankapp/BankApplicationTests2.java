d2dae6b6c2fde63acde1eb0b77efb5c8e3b7a233
Max <rukav77@gmail.com>
2022-07-15 10:18:20 +0500

    ex6_task4 tests

--- src/test/java/com/luxoft/bankapp/BankApplicationTests2.java
+++ src/test/java/com/luxoft/bankapp/BankApplicationTests2.java
@@ -1,8 +1,18 @@
 package com.luxoft.bankapp;
 
+import com.luxoft.bankapp.model.Client;
+import com.luxoft.bankapp.service.storage.ClientRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = BankApplication.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
@@ -11,48 +21,48 @@ public class BankApplicationTests2 {
     private static final String[] CLIENT_NAMES =
             {"Jonny Bravo", "Adam Budzinski", "Anna Smith"};
 
-//    @Autowired
-//    private ClientRepository clientRepository;
-//
-//    @Test
-//    public void storageBeanConfiguration() {
-//        assertNotNull(clientRepository, "storage bean should be configured");
-//        assertTrue((clientRepository instanceof ClientRepository), "storage should be instantiated with ClientCrudStorage class");
-//    }
-//
-//    @Test
-//    public void findClientsSortedByName() {
-//        List<Client> clients = clientRepository.findAll(Sort.by("name"));
-//
-//        assertEquals(3, clients.size());
-//        assertEquals(CLIENT_NAMES[1], clients.get(0).getName());
-//        assertEquals(CLIENT_NAMES[2], clients.get(1).getName());
-//        assertEquals(CLIENT_NAMES[0], clients.get(2).getName());
-//    }
-//
-//    @Test
-//    public void findClientsSortedByCity() {
-//        List<Client> clients = clientRepository.findAll(Sort.by("city"));
-//
-//        assertEquals(CLIENT_NAMES[1], clients.get(0).getName());
-//        assertEquals(CLIENT_NAMES[0], clients.get(1).getName());
-//        assertEquals(CLIENT_NAMES[2], clients.get(2).getName());
-//    }
-//
-//    @Test
-//    public void findClientsPage1() {
-//        List<Client> clients = clientRepository.findAll(PageRequest.of(0, 2)).toList();
-//
-//        assertEquals(2, clients.size());
-//        assertEquals(CLIENT_NAMES[1], clients.get(0).getName());
-//        assertEquals(CLIENT_NAMES[2], clients.get(1).getName());
-//    }
-//
-//    @Test
-//    public void findClientsPage2() {
-//        List<Client> clients = clientRepository.findAll(PageRequest.of(1, 2)).toList();
-//
-//        assertEquals(1, clients.size());
-//        assertEquals(CLIENT_NAMES[0], clients.get(0).getName());
-//    }
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Test
+    public void storageBeanConfiguration() {
+        assertNotNull(clientRepository, "storage bean should be configured");
+        assertTrue((clientRepository instanceof ClientRepository), "storage should be instantiated with ClientCrudStorage class");
+    }
+
+    @Test
+    public void findClientsSortedByName() {
+        List<Client> clients = clientRepository.findAll(Sort.by("name"));
+
+        assertEquals(3, clients.size());
+        assertEquals(CLIENT_NAMES[1], clients.get(0).getName());
+        assertEquals(CLIENT_NAMES[2], clients.get(1).getName());
+        assertEquals(CLIENT_NAMES[0], clients.get(2).getName());
+    }
+
+    @Test
+    public void findClientsSortedByCity() {
+        List<Client> clients = clientRepository.findAll(Sort.by("city"));
+
+        assertEquals(CLIENT_NAMES[1], clients.get(0).getName());
+        assertEquals(CLIENT_NAMES[0], clients.get(1).getName());
+        assertEquals(CLIENT_NAMES[2], clients.get(2).getName());
+    }
+
+    @Test
+    public void findClientsPage1() {
+        List<Client> clients = clientRepository.findAll(PageRequest.of(0, 2)).toList();
+
+        assertEquals(2, clients.size());
+        assertEquals(CLIENT_NAMES[1], clients.get(0).getName());
+        assertEquals(CLIENT_NAMES[2], clients.get(1).getName());
+    }
+
+    @Test
+    public void findClientsPage2() {
+        List<Client> clients = clientRepository.findAll(PageRequest.of(1, 2)).toList();
+
+        assertEquals(1, clients.size());
+        assertEquals(CLIENT_NAMES[0], clients.get(0).getName());
+    }
 }
